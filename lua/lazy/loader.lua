f04dd86a31c9f4a7ef750f6c005e5f07cbc0b6cd
fgdergewrgegr <afrakov10@gmail.com>
2026-02-15 18:00:14 +0300

    ~/

--- lua/lazy/loader.lua
+++ lua/lazy/loader.lua
@@ -1,17 +1,3 @@
-local lt, lpath = {}, "/home/vhck/.config/nvim/lua/lazy"
-local ppath, spath = lpath.."/plugins", lpath.."/setups"
-
-local handle = vim.loop.fs_scandir(ppath)
-while true do
-	local name, type = vim.loop.fs_scandir_next(handle)
-	if not name then break end
-	if type == "file" and name:match("%.lua$") then
-		local file_path = ppath .. "/" .. name
-		local spec = dofile(file_path)
-		lt[spec[1]] = spec
-	end
-end
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
@@ -24,6 +10,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
+
+local lt, lpath = {}, "~/.config/nvim/lua/lazy"
+local ppath, spath = lpath.."/plugins", lpath.."/setups"
+
+local handle = vim.loop.fs_scandir(ppath)
+while true do
+	local name, type = vim.loop.fs_scandir_next(handle)
+	if not name then break end
+	if type == "file" and name:match("%.lua$") then
+		local file_path = ppath .. "/" .. name
+		local spec = dofile(file_path)
+		lt[spec[1]] = spec
+	end
+end
 
 for _,plg in next,require("lazy.others") do 
 	if not(lt[plg[1]]) then 
